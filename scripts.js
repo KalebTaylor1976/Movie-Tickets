c336d4884bc4c761743c17afc0c05c5978c9587b
Kaleb Taylor <kaleb.taylor1976@gmail.com>
2023-02-14 20:04:59 -0800

    add checks to business logic

--- scripts.js
+++ scripts.js
@@ -6,3 +6,21 @@ function Ticket(movieName, timeOfDay, age) {
   this.age = age;
 }
 
+Ticket.prototype.calculatePrice = function() {
+  const BASE_PRICE = 10;
+  const MATINEE_PRICE = 8;
+  const SENIOR_DISCOUNT = 0.2; // 20% discount for seniors
+  let price = BASE_PRICE;
+
+  // Check for matinee price
+  if (this.timeOfDay === 'matinee') {
+    price = MATINEE_PRICE;
+  }
+
+  // Check for senior discount
+  if (this.age >= 65) {
+    price = price * (1 - SENIOR_DISCOUNT);
+  }
+
+  return price;
+};
